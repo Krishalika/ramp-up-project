57eadfe14870851ea7b57eb57f750e1cba038fc7
KrishalikaR <krishalika.r@synergenhealth.com>
2022-04-18 19:52:11 +0530

    installed dependencies

--- student-management-frontend/src/app/app.module.ts
+++ student-management-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataGridComponent } from './student-data-module/components/data-grid/data-grid.component';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    BrowserAnimationsModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
