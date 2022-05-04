75193133bedffd355cfe04083fb7d7ce1ff97cc3
Krishalika.R <krishalika.r@synergenhealth.com>
2022-05-04 15:56:38 +0530

    few changes

--- student-management-backend/apps/notification/src/notification.controller.ts
+++ student-management-backend/apps/notification/src/notification.controller.ts
@@ -2,19 +2,33 @@ import { Controller, Get } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { CreateStudentEvent } from 'apps/student-management-backend/src/student/events/create-student.event';
 import { UploadFileEvent } from './events/upload-file.event';
+import { NotificationGateway } from './notification.gateway';
 import { NotificationService } from './notification.service';
 
 @Controller()
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(
+    private readonly notificationService: NotificationService,
+    private readonly gateway: NotificationGateway,
+  ) {}
 
   @EventPattern('student_created')
   handleStudentCreated(data: CreateStudentEvent) {
     this.notificationService.handleStudentCreated(data);
   }
 
+  // @EventPattern('file_processed')
+  // handleFileProcessed(data: UploadFileEvent) {
+  //   this.notificationService.handleFileProcessed(data);
+  // }
+
   @EventPattern('file_processed')
   handleFileProcessed(data: UploadFileEvent) {
-    this.notificationService.handleFileProcessed(data);
+    return this.notificationService.handleFileProcessed(data);
+  }
+
+  @Get('test')
+  handle(client: any, data: any) {
+    client.emit('test', this.handleFileProcessed);
   }
 }
--- student-management-backend/apps/notification/src/notification.gateway.ts
+++ student-management-backend/apps/notification/src/notification.gateway.ts
@@ -10,7 +10,9 @@ import { Logger } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 
 @WebSocketGateway({ cors: true })
-export class NotificationGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
+export class NotificationGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer() server;
 
   private logger: Logger = new Logger('NotificationService');
@@ -18,20 +20,17 @@ export class NotificationGateway implements OnGatewayInit, OnGatewayConnection, 
   constructor(private service: NotificationService) {}
 
   handleConnection(client: any, ...args: any[]) {
-    console.log("Connected");
-    
+    console.log('Connected');
   }
   handleDisconnect(client: any) {
-    console.log("disconnected");
-    
+    console.log('disconnected');
   }
   afterInit(server: any) {
-    console.log("initiated");
-    
+    console.log('initiated');
   }
 
-  @SubscribeMessage("test")
-  handleFileProcessed(client:any, data:any) {
+  @SubscribeMessage('test')
+  handleFileProcessed(client: any, data: any) {
     try {
       this.server.emit('messages', data);
     } catch (e) {
@@ -39,7 +38,7 @@ export class NotificationGateway implements OnGatewayInit, OnGatewayConnection, 
     }
   }
 
-  handleStudentCreated() {
-    this.server.emit('client', this.service.handleStudentCreated);
-  }
+  // handleStudentCreated() {
+  //   this.server.emit('client', this.service.handleStudentCreated);
+  // }
 }
